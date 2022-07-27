fc11e82eda1909de0ec394dc32804fdedcbc76b3
aswathm78 <aswathm78@gmail.com>
2022-07-27 21:10:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xvq5hl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xvq5hl)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
